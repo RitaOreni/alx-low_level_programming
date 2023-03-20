f0de6f4cbd2165864297b336ffbb14f7070b6b54
RitaOreni <orenirita23@gmail.com>
2023-03-20 07:20:11 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers from 1-10 followed by a new line.
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		printf("%d", n);
+	}
+		printf("\n");
+	return (0);
+}
+
